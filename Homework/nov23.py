2fefe321c6195e3b4eb5457fc409a5b167acee42
Fupeng Zhao <72841750+ConnerZhao@users.noreply.github.com>
2021-11-23 12:30:16 -0800

    nov 23

---
+++ Homework/nov23.py
@@ -0,0 +1,20 @@
+"""
+Requirements:
+if conditions
+casting
+string concatenation
+inputs
+outputs
+"""
+
+# Reference: https://www.w3schools.com/python/python_conditions.asp
+
+# user input name
+
+# user input age
+
+# if greater or equal to 18
+    # output username is an adult
+
+# else 
+    # output username is not an adult
---
+++ main.py
@@ -0,0 +1,4 @@
+x = "Hello World"
+print('"Hello World" - Fupeng Zhao')
+print("'Hello World again' - Not by Fupeng Zhao")
+print("Fupeng said: \"Hey\"")
