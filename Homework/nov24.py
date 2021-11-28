42a28609529b5a1b35af68fee25d5749e0a0db79
Fupeng Zhao <72841750+ConnerZhao@users.noreply.github.com>
2021-11-28 12:30:56 -0800

    nov 28

--- Homework/nov24.py
+++ Homework/nov24.py
@@ -1,4 +1,16 @@
-# Two var each assigned random number
-# Prompt the numbers
-# * / % + -
-# Have the user anwer the question
+x = int(19)
+y = int(578)
+n = x + y # Answer
+print("What is " + str(x) + " + " + str(y) + " = ?")
+userInput = int(input("Enter answer here: "))
+numberOfTrials = 1
+
+while numberOfTrials != 3:
+    if (userInput == n):
+        print("Correct")
+        break
+    else:
+        print("Incorrect")
+        print("You have attempted " + str(numberOfTrials) + " times")
+        numberOfTrials += 1
+        userInput = int(input("Enter answer here: "))
--- Homework/nov28.py
+++ Homework/nov28.py
@@ -1,3 +1,9 @@
+'''
+using while loop
+find all odd number from 1 to 100
+find the sum of all odd number from 1 to 100
+'''
+# Reference:
 currentNumber = 0
 total = 0 # Init the var
 while currentNumber != 100:
