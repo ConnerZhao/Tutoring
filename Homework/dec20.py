36d8534d9803debe83a4d3e5044a32ace42fb853
Fupeng Zhao <72841750+ConnerZhao@users.noreply.github.com>
2021-12-20 12:04:56 -0800

    dec 20

--- Homework/dec20.py
+++ Homework/dec20.py
@@ -1,13 +1,10 @@
 import random #make computer smarter
-
 x = random.randint(0,10) # generates a random number between a, b (0, 10)
 z = int(input("Guess the number(0 to 10): ")) # Initial input
-
 # Checks if the user inputted a valid response
 while not((z <= 10) and (z >= 0)): # Condition checking, if the user's input fits the parameter
     print("Overflow, try again")
     z = int(input("Guess the number(0 to 10): ")) # Try again
-
 # Actual guessing part
 while z != x:
     if x > z:
@@ -16,3 +13,12 @@ while z != x:
     elif x < z:
         print("Too high")
         z = int(input("Guess the number(0 to 10): ")) # Try again
+
+
+'''
+Computer Guessing game
+have the computer guess a number you are thinking
+between 0 to 10
+the computer will ask if its response is too high or too low
+USE GOOGLE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+'''
