951d91dfaf5ffcbe42fa496664cd30484caa2bac
Fupeng Zhao <72841750+ConnerZhao@users.noreply.github.com>
2022-01-05 11:55:29 -0800

    jan 5th

---
+++ Homework/jan5.py
@@ -0,0 +1,11 @@
+'''
+two lists
+    1 is the student's schedule
+    2 available classes to take
+        out put the available classes as a list
+    ask the student which class to take
+    input a number for the class they want to take
+
+    INPUT from student which period they want to take it
+    insert the class according to the student's selection
+'''
